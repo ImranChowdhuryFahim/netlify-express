ee734a3e1a79bf25225daa6d46a11c7b9f37eb61
Imran <u1704107@student.cuet.ac.bd>
2020-07-07 04:18:12 +0600

    author

--- express/server.js
+++ express/server.js
@@ -16,6 +16,7 @@ router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/author',(req,res)=>{res.send("<h1>asci ami arek page e</h1>")})
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
